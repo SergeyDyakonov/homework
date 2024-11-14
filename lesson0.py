cf5b608918d012657840782f3612691295284725
Сергей <dyakonovsergey@gmail.com>
2024-11-14 11:29:59 +0300

    09.11

---
+++ lesson0.py
@@ -0,0 +1,9 @@
+completed_tasks = 12
+print(completed_tasks)
+hours_spent = 1.5
+print(hours_spent)
+course_name = "Python"
+print(course_name)
+time_for_one_task = (hours_spent / completed_tasks)
+print(time_for_one_task)
+print("Курс:", course_name,",","всего задач:",completed_tasks,"," "затрачено часов:",hours_spent,",","среднее время выполнения", time_for_one_task, "часа.")
---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Сергей"
+print(name)
+age = 40
+print(age)
+new_age = 1
+print(age + new_age)
+name = "is_student"
+print(bool(name))
